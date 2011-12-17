8706ff28c5af5b4dedd5e6d5a31d7c12d5ebb414
Jonathan Cran <jcran@0x0e.org>
2011-12-16 20:53:28 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ear::Application.config.secret_token = 'b3b6d83128cfed51052c67067c799b05dddbb9cdb9dcd8e453341046919a292cefbc977e02456c5346e95802341320b7f52a3d7bb5ac6164f961284c5532e152'
