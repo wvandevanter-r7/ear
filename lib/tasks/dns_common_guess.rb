13e3aa96d72df8749ce4e16da5835c71823504cc
Jonathan Cran <jcran@0x0e.org>
2012-01-23 00:30:09 -0600

    add a simple task to try a couple common domains for an organization

---
+++ lib/tasks/dns_common_guess.rb
@@ -0,0 +1,59 @@
+require 'dnsruby'
+
+def name
+  "dns_common_guess"
+end
+
+## Returns a string which describes what this task does
+def description
+  "Guess some common domain names based on the organization name"
+end
+
+## Returns an array of valid types for this task
+def allowed_types
+  [Organization]
+end
+
+def setup(object, options={})
+  super(object, options)
+end
+
+def run
+  super
+
+  @task_logger.log "Running DNS name guess on #{@object}"
+
+  domains = ["#{@object.name}.com", "#{@object.name}.net", "#{@object.name}.org"]
+
+  # For each of the suggested domains
+  domains.each do |domain|
+    begin
+      res_answer = Resolv.new.getaddress(domain)
+      if res_answer
+        @task_logger.log_good "DNS Guess succeeded for #{res_answer}"
+        @task_run.save_raw_result "domain: #{res_answer}"
+
+        # We know the domain is ~valid, and the ip address too
+        d = create_object(Domain, :name => domain)
+        h = create_object(Host, :ip_address => res_answer)
+
+        # Associate
+        @object.domains << d if d
+        d.hosts << h if h
+
+    end
+    rescue Dnsruby::Refused
+      @task_logger.log "Zone Transfer against #{@object.name} refused."
+    rescue Dnsruby::ResolvError
+      @task_logger.log "Unable to resolve #{@object.name}"
+    rescue Dnsruby::ResolvTimeout
+      @task_logger.log "Timed out while querying #{@object.name}."
+    rescue Exception => e
+      @task_logger.log "Unknown exception: #{e}"
+    end
+  end
+end
+
+def cleanup
+  super
+end
